36943788418ad72972df4d68f68d4fd23e391e27
Andrey Akimov <hi.andrey.akimov@gmail.com>
2023-11-27 21:54:37 +0600

    feat: [IDLE-2] Настроена Chakra UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import { render } from "react-dom";
 import './index.scss';
 import { StrictMode } from "react";
+import {ChakraProvider, Progress} from "@chakra-ui/react";
 
 render(
   <StrictMode>
-    <div className='app'></div>
+      <ChakraProvider>
+          <div className='app'>
+              <Progress value={20} size='xs' colorScheme='pink' />
+          </div>
+      </ChakraProvider>
   </StrictMode>,
   document.getElementById('root'),
 );
